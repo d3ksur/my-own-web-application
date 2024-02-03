4c3e7b9060510bb848f840f2807a1636ac1aa637
Franco Reyes <d3ksur@Francos-MacBook-Pro.local>
2024-02-03 20:52:18 -0300

    master auto-commit

--- src/app/components/promise-observable-use/promise-observable-use.component.ts
+++ src/app/components/promise-observable-use/promise-observable-use.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-promise-observable-use',
@@ -15,13 +15,37 @@ export class PromiseObservableUseComponent {
   public constructor(private http: HttpClient) {}
 
   public promiseCallback(): void {
+    // Esto es una promesa la cual imprime 1 + 1 en la consola despues de 2 segundos
     let promise: Promise<number> = new Promise<number>((resolve) => {
       setTimeout(() => resolve(1 + 1), 2000);
     });
     promise.then(console.log);
+
+    // Esto es un observable el cual va sumando 1 por segundo y despues de 5 segundo se corta
+    let observable = new Observable<number>((observer) => {
+      let count = 1;
+      let interval = setInterval(() => {
+        observer.next(count++);
+      }, 1000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    });
+
+    let subscription = observable.subscribe((value) => {
+      console.log(value);
+    });
+
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 5000);
   }
 
-  public getPokemons(pokemonName: string): Subscription {
+  public getPokemons(pokemonName: string): Subscription | undefined {
+    if (this.selectedPokemon === '') {
+      return;
+    }
     return this.http
       .get(this.pokemonApi + pokemonName)
       .subscribe((data: any) => {
